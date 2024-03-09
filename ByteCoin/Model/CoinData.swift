51516136b52217855386241d71a02fb79ce53dea
deshollow <555demin@gmail.com>
2024-03-09 20:15:35 +0300

    Initial Commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by deshollow on 09.03.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
